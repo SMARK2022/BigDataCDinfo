a131e7635d00cb19e2c385ef1a32b6044304db29
SMARK <3222301373@qq.com>
2024-07-15 12:16:25 +0800

    new file:   .idea/.gitignore 	new file:   .idea/BigDataCDinfo.iml 	new file:   .idea/misc.xml 	new file:   .idea/modules.xml 	new file:   .vscode/settings.json 	new file:   "Dask\346\226\271\346\241\210.ipynb" 	new file:   IDEA/.idea/.gitignore 	new file:   IDEA/.idea/IDEA.iml 	new file:   IDEA/.idea/misc.xml 	new file:   IDEA/.idea/modules.xml 	new file:   Old/2022First final00.scala 	new file:   Old/2022First final01.scala 	new file:   Old/2022First update copy.scala 	new file:   Old/2022First update.scala 	new file:   Old/2022First.scala 	new file:   Old/2023First.scala 	new file:   Strange5273/.idea/.gitignore 	new file:   Strange5273/.idea/artifacts/Strange5273.xml 	new file:   Strange5273/.idea/codeStyles/Project.xml 	new file:   Strange5273/.idea/codeStyles/codeStyleConfig.xml 	new file:   Strange5273/.idea/libraries/jars.xml 	new file:   Strange5273/.idea/misc.xml 	new file:   Strange5273/.idea/modules.xml 	new file:   Strange5273/.idea/uiDesigner.xml 	new file:   Strange5273/Strange5273.iml 	new file:   Strange5273/out/production/Strange5273/Strange5273$$typecreator5$1.class 	new file:   Strange5273/out/production/Strange5273/Strange5273$$typecreator6$1.class 	new file:   Strange5273/out/production/Strange5273/Strange5273$.class 	new file:   Strange5273/out/production/Strange5273/Strange5273.class 	new file:   Strange5273/src/Strange5273.scala 	new file:   Update5273/.idea/.gitignore 	new file:   Update5273/.idea/artifacts/Update5273.xml 	new file:   Update5273/.idea/codeStyles/Project.xml 	new file:   Update5273/.idea/codeStyles/codeStyleConfig.xml 	new file:   Update5273/.idea/libraries/jars.xml 	new file:   Update5273/.idea/misc.xml 	new file:   Update5273/.idea/modules.xml 	new file:   Update5273/Update5273.iml 	new file:   Update5273/out/production/Update5273/Update5273$$typecreator5$1.class 	new file:   Update5273/out/production/Update5273/Update5273$$typecreator6$1.class 	new file:   Update5273/out/production/Update5273/Update5273$.class 	new file:   Update5273/out/production/Update5273/Update5273.class 	new file:   Update5273/src/Update5273.scala 	new file:   demo.ipynb 	new file:   infected01.txt 	new file:   infected2.txt 	new file:   redmark.txt 	new file:   redmark/redmark01.txt 	new file:   redmark/redmarkA03.txt 	new file:   remark22.txt

---
+++ Old/2022First update copy.scala
@@ -0,0 +1,74 @@
+import org.apache.spark.sql.functions.{col, collect_list, struct, when, udf}
+import org.apache.spark.sql.{Row, SparkSession}
+import org.apache.spark.storage.StorageLevel
+
+object First2022_update {
+  def main(args: Array[String]): Unit = {
+    val spark = SparkSession.builder()
+      .appName("First2022_update")
+      .config("spark.serializer", "org.apache.spark.serializer.KryoSerializer") // 序列化优化
+      .getOrCreate()
+
+    val cdinfo = spark.read.csv(args(0))
+    val infected = spark.read.csv(args(1))
+    val infectedPersonIds = infected.select("_c0").rdd.map(row => row.getString(0)).collect().distinct
+
+    val filteredCdinfo = cdinfo.filter(col("_c3").isin(infectedPersonIds: _*))
+
+    val groupedCdinfo = filteredCdinfo.groupBy("_c0").agg(
+      collect_list(when(col("_c2") === 1, col("_c1")).cast("long")).alias("startTimes"),
+      collect_list(when(col("_c2") === 2, col("_c1")).cast("long")).alias("endTimes")
+    )
+
+    // 持久化 groupedCdinfo 数据
+    groupedCdinfo.persist(StorageLevel.MEMORY_AND_DISK_SER)
+
+    // 显式指定 collect 后的类型
+    val baseStationInfectedTimes: Map[String, (Seq[Long], Seq[Long])] = groupedCdinfo.collect().map { row =>
+      val baseId = row.getString(0)
+      val startTimes = row.getAs[Seq[Long]]("startTimes")
+      val endTimes = row.getAs[Seq[Long]]("endTimes")
+      (baseId, (startTimes, endTimes))
+    }.toMap
+
+
+    val infectedBaseStations = baseStationInfectedTimes.keys.toList
+    val potentiallyInfectedPeople = cdinfo.filter(col("_c0").isin(infectedBaseStations: _*))
+    potentiallyInfectedPeople.persist(StorageLevel.MEMORY_AND_DISK_SER)
+    val groupedPotentiallyInfected = potentiallyInfectedPeople.groupBy("_c0", "_c3").agg(
+      collect_list(col("_c1").cast("long")).alias("times")
+    )
+    groupedPotentiallyInfected.show(false)
+
+    def isInfected(baseId: String, times: Seq[Long]): Boolean = {
+      val sortedTimes = times.sorted
+      if (sortedTimes.nonEmpty && baseStationInfectedTimes.contains(baseId)) {
+        val (startTimes, endTimes) = baseStationInfectedTimes(baseId)
+        for (i <- startTimes.indices) {
+          val startTime = startTimes(i)
+          val endTime = endTimes(i)
+          if ((startTime <= sortedTimes.head && endTime >= sortedTimes.head) ||
+            (startTime <= sortedTimes.last && endTime >= sortedTimes.last) ||
+            (startTime >= sortedTimes.head && endTime <= sortedTimes.last)) {
+            return true
+          }
+        }
+      }
+      return false
+    }
+
+    // 注册UDF函数
+    val isInfectedUDF = udf(isInfected _)
+    val finalInfected = groupedPotentiallyInfected.filter(isInfectedUDF(col("_c0"), col("times")))
+    finalInfected.show()
+
+    val basicTask = potentiallyInfectedPeople.select("_c3").distinct().sort("_c3")
+    basicTask.coalesce(1).write.text(args(2))
+
+    val finalTask = finalInfected.select("_c3").distinct().sort("_c3")
+    finalTask.coalesce(1).write.text(args(3))
+    
+    groupedCdinfo.unpersist()
+    potentiallyInfectedPeople.unpersist()
+  }
+}
---
+++ Strange5273/src/Strange5273.scala
@@ -0,0 +1,98 @@
+import org.apache.spark.sql.functions.{col, collect_list, when, udf}
+import org.apache.spark.sql.{Row, SparkSession}
+import org.apache.spark.storage.StorageLevel
+
+object Strange5273 {
+  def main(args: Array[String]): Unit = {
+    val spark = SparkSession.builder()
+      .appName("First2022_update")
+      .config("spark.serializer", "org.apache.spark.serializer.KryoSerializer")
+      .getOrCreate()
+
+    import spark.implicits._
+
+    val cdinfo = spark.read.csv(args(0)).toDF("id", "timestamp", "type", "personId")
+    val infected = spark.read.csv(args(1)).toDF("personId")
+
+    val infectedPersonIds = infected.select("personId").as[String].collect().toSet
+    val infectedPersonIdsBC = spark.sparkContext.broadcast(infectedPersonIds)
+
+    // 过滤每个分区的数据，减少数据传输
+    val filteredCdinfo = cdinfo.filter(row => infectedPersonIdsBC.value.contains(row.getString(3)))
+
+    val groupedCdinfo = filteredCdinfo.groupBy("id").agg(
+      collect_list(when(col("type") === 1, col("timestamp")).cast("long")).alias("startTimes"),
+      collect_list(when(col("type") === 2, col("timestamp")).cast("long")).alias("endTimes")
+    )
+
+    // 持久化 groupedCdinfo 数据
+    groupedCdinfo.persist(StorageLevel.MEMORY_AND_DISK_SER)
+
+    // 显式指定 collect 后的类型，并确保 startTimes 和 endTimes 长度一致
+    val baseStationInfectedTimes: Map[String, (Seq[Long], Seq[Long])] = groupedCdinfo.collect().map { row =>
+      val baseId = row.getString(0)
+      val startTimes = row.getAs[Seq[Long]]("startTimes")
+      var endTimes = row.getAs[Seq[Long]]("endTimes")
+      if (startTimes.length != endTimes.length) {
+        endTimes = endTimes.drop(endTimes.length - startTimes.length)
+      }
+      (baseId, (startTimes, endTimes))
+    }.toMap
+
+    val baseStationInfectedTimesBC = spark.sparkContext.broadcast(baseStationInfectedTimes)
+
+    val potentiallyInfectedPeople = cdinfo.filter { row =>
+      val baseId = row.getString(0)
+      baseStationInfectedTimesBC.value.contains(baseId)
+    }
+
+    potentiallyInfectedPeople.persist(StorageLevel.MEMORY_AND_DISK_SER)
+
+    val groupedPotentiallyInfected = potentiallyInfectedPeople.groupBy("id", "personId").agg(
+      collect_list(col("timestamp").cast("long")).alias("times")
+    )
+    groupedPotentiallyInfected.show(false)
+
+    def isInfected(baseId: String, times: Seq[Long]): Boolean = {
+      val sortedTimes = times.sorted
+      // 如果 times 长度为奇数，直接返回 true
+      if (sortedTimes.length % 2 != 0) {
+        return true
+      }
+
+      if (sortedTimes.nonEmpty && baseStationInfectedTimesBC.value.contains(baseId)) {
+        val (startTimes, endTimes) = baseStationInfectedTimesBC.value(baseId)
+        for (i <- startTimes.indices) {
+          val startTime = startTimes(i)
+          val endTime = endTimes(i)
+          for (j <- sortedTimes.indices by 2) {
+            val entryTime = sortedTimes(j)
+            val exitTime = sortedTimes(j + 1)
+            if ((startTime <= entryTime && endTime >= entryTime) ||
+              (startTime <= exitTime && endTime >= exitTime) ||
+              (startTime >= entryTime && endTime <= exitTime)) {
+              return true
+            }
+          }
+        }
+      }
+      false
+    }
+
+
+
+    // 注册UDF函数
+    val isInfectedUDF = udf(isInfected _)
+    val finalInfected = groupedPotentiallyInfected.filter(isInfectedUDF(col("id"), col("times")))
+    finalInfected.show()
+
+    val basicTask = potentiallyInfectedPeople.select("personId").distinct().sort("personId")
+    basicTask.coalesce(1).write.text(args(2))
+
+    val finalTask = finalInfected.select("personId").distinct().sort("personId")
+    finalTask.coalesce(1).write.text(args(3))
+
+    groupedCdinfo.unpersist()
+    potentiallyInfectedPeople.unpersist()
+  }
+}
